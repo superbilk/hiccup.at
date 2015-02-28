c48d54a87e277d454116a26daaf49ef04fa758f1
Chris Schmidt <chris@superbilk.org>
2015-02-28 17:05:00 +0100

    see if you are attending (fake buttons)

--- client/templates/events/event_item.js
+++ client/templates/events/event_item.js
@@ -1,5 +1,10 @@
 Template.eventItem.helpers({
   attendantsCount: function() {
     return this.attendants.length.toString();
+  },
+  currentUserAttending: function() {
+    return _.find(this.attendants, function(attendant){
+      return attendant._id === Meteor.user()._id;
+    });
   }
 });
