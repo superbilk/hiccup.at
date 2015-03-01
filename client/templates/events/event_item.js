f9e5d52411c22ad2b1faf61b548a3deba51b1d1b
Chris Schmidt <chris@superbilk.org>
2015-03-01 01:29:46 +0100

    another working twitter widget

--- client/templates/events/event_item.js
+++ client/templates/events/event_item.js
@@ -45,27 +45,7 @@ Template.eventItem.events({
 });
 
 Template.eventItem.rendered = function() {
-  if (!window.twttr) {
-    window.twttr = (function(d, s, id) {
-      var js, fjs = d.getElementsByTagName(s)[0],
-        t = window.twttr || {};
-      if (d.getElementById(id)) return;
-      js = d.createElement(s);
-      js.id = id;
-      js.src = "https://platform.twitter.com/widgets.js";
-      fjs.parentNode.insertBefore(js, fjs);
-
-      t._e = [];
-      t.ready = function(f) {
-        t._e.push(f);
-      };
-
-      return t;
-    }(document, "script", "twitter-wjs"));
-  }
-  else {
-    setTimeout(function() {
-      window.twttr.widgets.load(this.firstNode);
-    }, 0);
-  };
+  setTimeout(function() {
+    window.twttr.widgets.load(this.firstNode);
+  }, 0);
 };
