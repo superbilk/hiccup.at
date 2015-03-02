de448ed29f782b350fa09846de499637bed4e49e
Chris Schmidt <chris@superbilk.org>
2015-03-02 21:30:12 +0100

    added datetimepicker

--- client/templates/events/event_edit.js
+++ client/templates/events/event_edit.js
@@ -1,6 +1,23 @@
 Template.eventEdit.rendered = function() {
-  $('#datetimepicker').datetimepicker({
-    defaultDate: moment(this.data.dateBegin).format("DD.MM.YYYY"),
-    format: "DD.MM.YYYY"
+  console.log(moment.utc(this.data.dateBegin).format());
+  $('#datetimepickerBegin').datetimepicker({
+    locale: 'de',
+    // minDate: moment(),
+    sideBySide: true,
+    stepping: 15,
+    defaultDate: moment.utc(this.data.dateBegin).format()
+  });
+  $('#datetimepickerEnd').datetimepicker({
+    locale: 'de',
+    // minDate: moment(),
+    sideBySide: true,
+    stepping: 15,
+    defaultDate: moment.utc(this.data.dateEnd).format()
+  });
+  $("#datetimepickerBegin").on("dp.change", function (e) {
+    $('#datetimepickerEnd').data("DateTimePicker").minDate(e.date);
+  });
+  $("#datetimepickerEnd").on("dp.change", function (e) {
+    $('#datetimepickerBegin').data("DateTimePicker").maxDate(e.date);
   });
 };
