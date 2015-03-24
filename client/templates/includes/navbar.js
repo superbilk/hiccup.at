6cd761f583e0ab8c658ff0737b80da069f4f20b2
Chris Schmidt <chris@superbilk.org>
2015-03-24 14:52:58 +0100

    language switcher

--- client/templates/includes/navbar.js
+++ client/templates/includes/navbar.js
@@ -1,6 +1,20 @@
 Template.navbar.helpers({
   allEvents: function() {
     return Events.find({}, { sort: { dateBegin: -1 }} );
-    // return Events.find();
   }
 });
+
+Template.navbar.events({
+  'click a.lang': function(event){
+    event.preventDefault();
+    TAPi18n.setLanguage(this.tag);
+  }
+});
+
+// Template.i18n_navbar_dropdown.helpers({
+//   langActive: function(cur) {
+//     console.log(TAPi18n.getLanguage());
+//     console.log(cur);
+//     return (TAPi18n.getLanguage() === cur)?'active':'';
+//   }
+// });
