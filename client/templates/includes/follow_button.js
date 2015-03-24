725b2a3b8ad5802bda5de92a3d30bbbf8c04445e
Chris Schmidt <chris@superbilk.org>
2015-03-24 18:39:57 +0100

    switched to bootswatch/paper

--- client/templates/includes/follow_button.js
+++ client/templates/includes/follow_button.js
@@ -1,6 +1,8 @@
 // http://stackoverflow.com/questions/16100123/embed-javascript-widget-in-meteor-template
 Template.followButton.rendered = function() {
   setTimeout(function() {
-    twttr.widgets.load(this.firstNode);
+    if (typeof twttr !== "undefined") {
+      twttr.widgets.load(this.firstNode);
+    };
   }, 0);
 };
