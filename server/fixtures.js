8c80046099b3c335df952a3c6eec0a4062a0a40d
Chris Schmidt <chris@superbilk.org>
2015-03-25 15:31:38 +0100

    updated fixtures

--- server/fixtures.js
+++ server/fixtures.js
@@ -34,6 +34,7 @@ Meteor.startup(function () {
       url: 'http://webworker-nrw.de',
       twitterOrganizer: "wwnrw",
       twitterOrganizerImage: "http://webworker-nrw.de/assets/images/WWNRW.png",
+      hashtag: "wwnrw",
       logoImage: "http://webworker-nrw.de/assets/images/WWNRW.png",
       dateBegin: new Date("2015-06-07T18:00:00Z"),
       dateEnd: new Date("2015-06-07T21:00:00Z"),
@@ -57,6 +58,7 @@ Meteor.startup(function () {
       url: 'http://leandus.de',
       twitterOrganizer: "Lean_DUS",
       twitterOrganizerImage: "https://www.leanuxdus.de/files/2015/01/logo-small-leandus.png",
+      hashtag: "LeanDUS",
       logoImage: "https://www.leanuxdus.de/files/2015/01/logo-small-leandus.png",
       dateBegin: new Date("2015-04-07T17:00:00Z"),
       dateEnd: new Date("2015-04-07T21:00:00Z"),
@@ -85,6 +87,7 @@ Meteor.startup(function () {
       url: 'http://leanuxdus.de',
       twitterOrganizer: "LeanUXDUS",
       twitterOrganizerImage: "https://www.leandus.de/files/2015/01/logo-small-leanuxdus.png",
+      hashtag: "LeanUXDUS",
       logoImage: "https://www.leandus.de/files/2015/01/logo-small-leanuxdus.png",
       dateBegin: new Date("2015-08-07T17:00:00Z"),
       dateEnd: new Date("2015-08-07T21:00:00Z")
@@ -96,6 +99,7 @@ Meteor.startup(function () {
       url: 'http://leanuxdus.de',
       twitterOrganizer: "LeanUXDUS",
       twitterOrganizerImage: "https://www.leandus.de/files/2015/01/logo-small-leanuxdus.png",
+      hashtag: "LeanUXDUS",
       logoImage: "https://www.leandus.de/files/2015/01/logo-small-leanuxdus.png",
       dateBegin: new Date("2015-03-01T17:00:00Z"),
       dateEnd: new Date("2015-03-21T21:00:00Z")
@@ -109,6 +113,7 @@ Meteor.startup(function () {
       url: 'http://leandus.de',
       twitterOrganizer: "Lean_DUS",
       twitterOrganizerImage: "https://www.leanuxdus.de/files/2015/01/logo-small-leandus.png",
+      hashtag: "LeanDUS",
       logoImage: "https://www.leanuxdus.de/files/2015/01/logo-small-leandus.png",
       dateBegin: new Date("2015-03-25T17:00:00Z"),
       dateEnd: new Date("2015-03-25T21:00:00Z"),
@@ -120,6 +125,7 @@ Meteor.startup(function () {
       url: 'http://www.meetup.com/Meteor-Cologne/events/220454449/',
       twitterOrganizer: "DerMambo",
       twitterOrganizerImage: "https://pbs.twimg.com/profile_images/378800000123289551/1f63258a55d204a327ac2b5a1bf1d673_400x400.jpeg",
+      hashtag: "meteorcologne",
       logoImage: "http://photos1.meetupstatic.com/photos/event/9/b/d/e/global_425859902.jpeg",
       dateBegin: new Date("2015-03-12T18:00:00 Z"),
       dateEnd: new Date("2015-03-12T21:00:00 Z")
@@ -131,6 +137,7 @@ Meteor.startup(function () {
       url: 'http://www.meetup.com/Meteor-Paris/events/220498616/',
       twitterOrganizer: "meteorjs",
       twitterOrganizerImage: "https://pbs.twimg.com/profile_images/527039622434418688/uJPMDhZq_400x400.png",
+      hashtag: "meteorparis",
       logoImage: "http://photos4.meetupstatic.com/photos/event/b/2/9/0/global_416205712.jpeg",
       dateBegin: new Date("2015-03-05T18:00:00 Z"),
       dateEnd: new Date("2015-03-05T21:30:00 Z")
@@ -142,6 +149,7 @@ Meteor.startup(function () {
       url: 'http://beyondtellerrand.com',
       twitterOrganizer: "btconf",
       twitterOrganizerImage: "https://pbs.twimg.com/profile_images/478502156589617152/7WDu9XsV.jpeg",
+      hashtag: "btconf",
       logoImage: "https://pbs.twimg.com/profile_images/478502156589617152/7WDu9XsV.jpeg",
       dateBegin: new Date("2015-05-11T09:00:00Z"),
       dateEnd: new Date("2015-05-13T16:00:00Z"),
@@ -153,6 +161,7 @@ Meteor.startup(function () {
       url: 'http://www.meetup.com/Ansible-Meetup-Dusseldorf/events/220945485/',
       twitterOrganizer: "dictvm",
       twitterOrganizerImage: "https://pbs.twimg.com/profile_images/512014618227523584/E_3IuDgo_400x400.jpeg",
+      hashtag: "ansibleDUS",
       logoImage: "http://cdn2.hubspot.net/hub/330046/file-769078210-png/Official_Logos/ansible_logo_black_square_small.png",
       dateBegin: new Date("2015-03-18T18:00:00 Z"),
       dateEnd: new Date("2015-03-18T21:00:00 Z")
@@ -164,6 +173,7 @@ Meteor.startup(function () {
       url: 'http://barcamp.berlin',
       twitterOrganizer: "barcampBER",
       twitterOrganizerImage: "https://pbs.twimg.com/profile_images/547158085538570240/u4DxIK6H_400x400.png",
+      hashtag: "barcampBER",
       logoImage: "https://pbs.twimg.com/profile_images/547158085538570240/u4DxIK6H_400x400.png",
       dateBegin: new Date("2015-03-07T09:00:00 Z"),
       dateEnd: new Date("2015-03-08T17:00:00 Z")
